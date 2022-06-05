3b113de472f82ea8ceba975c0e5f7d4b3bd16027
hanwang <hanwangx@gmail.com>
2022-06-05 23:39:01 +0100

    chore: adding netlify cms

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -30,6 +30,19 @@ const BlogPostTemplate = ({ data, pageContext }) => {
           <MyGitalk id={pageContext.slug} title={post.frontmatter.title} />
         )}
       </Main>
+      <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      <script>
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    })
+  }
+</script>
+      
     </Layout>
   )
 }
